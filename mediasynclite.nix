692334a3a79aa8b2740690a7d5695b81e5e79c52
Tobi Oloke <tobzdaman619@gmail.com>
2023-12-07 09:47:48 +0000

    missed a comma

---
+++ mediasynclite.nix
@@ -0,0 +1,25 @@
+{ pkgs ? import <nixpkgs> {} }: 
+
+
+pkgs.stdenv.mkDerivation rec {
+  pname = "mediasynclite";
+  version = "0.4.2";
+
+  src = pkgs.fetchgit {
+    url = "https://github.com/iBroadcastMediaServices/MediaSyncLiteLinux.git";
+    rev = "f215b082e7c5a2619503b073c27b9eda91a04275";
+    sha256 = "1c5iglf00askqv65x40lipzkkrjxvwcm6ip1wsbgccd14qk74553";
+    };
+
+  buildInputs = with pkgs; [
+   gtk3
+   pkg-config
+   curl
+   openssl.dev
+   jansson
+  ];
+
+  makeFlags = [ "PREFIX=$(out)" ];
+
+  pathsToLink = [ "share/ui/" "/bin" ];
+}
--- src/ibmsl.c
+++ src/ibmsl.c
@@ -668,7 +668,7 @@ int main (int argc, char **argv) {
         "share/ui/",
         "/usr/local/share/mediasynclite/",
         "/usr/share/mediasynclite/",
-        "$out/share/mediasynclite/"
+        "$out/share/mediasynclite/",
         NULL
     };
 
